de238a60bf25bb86414d32acc04e50178e8fd6a3
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-10 21:41:21 +0100

    Created Sidebar and starting on navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './home.scss';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="home">
+            <Sidebar />
+            <div className='home-container'>
+                    
+            </div>
         </div>
     );
 }
