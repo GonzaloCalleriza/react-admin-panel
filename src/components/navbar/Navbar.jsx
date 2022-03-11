2d944bf3f90d09b41e6ae12bad0f47c0b4a556b4
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-03-11 10:01:24 +0100

    Fixed mistakes of redundant lines from template

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import './navbar.scss';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlinedIcon';
-import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlinedIcon';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlinedIcon';
-import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlinedIcon';
-import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlinedIcon';
-import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlinedIcon';
-import ListOutlinedIcon from '@mui/icons-material/ListOutlinedIcon';
+import "./navbar.scss";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 const Navbar = () => {
     return (
